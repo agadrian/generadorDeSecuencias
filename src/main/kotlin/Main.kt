dad8d21cdbcc320588bcd0669fede2cbda18af64
aarrgom935 <aarrgom935@g.educaand.es>
2024-04-03 14:42:49 +0200

    GeneradorSecuencias

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,9 @@
-// Hacer un menu y pasarle la consola en el construcotr (IConsola)
 
-// usar .tolist() ,  .jointostrinig etc
 fun main(args: Array<String>) {
     val consola = Consola()
     val generadorSecuencia = GeneradorSecuencia(consola)
     val menu = Menu(generadorSecuencia, consola)
-    menu.menuPrincipal()
 
+    menu.menuPrincipal()
 
 }
